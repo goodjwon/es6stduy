b970161df3767acbcb01070956eef20dc5eb0dd6
goodjwon <jwon76@naver.com>
2019-02-05 14:08:39 +0900

    destructuing json 예제 추가

--- destructuring.js
+++ destructuring.js
@@ -13,3 +13,36 @@ console.log(name, age);
 
 let {name:myName, age:myAge} = obj;
 console.log(myName, myAge)
+;
+
+//json data 바로 바인딩 하기.
+const jsonmessage = [
+    {
+        "postTitle": "mbc",
+        "postWriter": "담당자",
+        "postUserId": "guest",
+        "postContent": "냉무냉무냉무",
+        "postPassword": null,
+        "postUserEmail": "admin@naver.com",
+        "postsCode": "board",
+        "postsDomainCode": "abc",
+        "postIpAddress": "58.225.21.444",
+        "postGroup": "testPostsParents",
+    },
+    {
+        "postTitle": "kbs",
+        "postWriter": "담당자",
+        "postUserId": "guest",
+        "postContent": "냉무냉무냉무",
+        "postPassword": null,
+        "postUserEmail": "admin@naver.com",
+        "postsCode": "board",
+        "postsDomainCode": "abc",
+        "postIpAddress": "58.225.21.444",
+        "postGroup": "testPostsParents",
+    }]
+;
+
+
+let [,{postTitle, postWriter}] = jsonmessage;
+console.log(postTitle, postWriter)
